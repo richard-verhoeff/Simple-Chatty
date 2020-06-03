3673d397ecb87f6e040d856c321455a12be1f420
richard-verhoeff <66055866+richard-verhoeff@users.noreply.github.com>
2020-06-03 10:05:16 +0200

    Initial commit

---
+++ Problems/Minimum and maximum/task.py
@@ -0,0 +1,12 @@
+num1 = int(input())
+num2 = int(input())
+if num1 < num2:
+    if num1 > num2:
+        print(num1)
+        print(num2)
+    else:
+        print(num2)
+        print(num1)
+else:
+    print(num1)
+    print(num2)
